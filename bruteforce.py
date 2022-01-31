797a65d2109bb29cca9f6742301c6fd3c207ec7e
byoso <emailvinz@gmail.com>
2022-01-31 19:03:13 +0100

    Algorithmes are working fine

--- bruteforce.py
+++ bruteforce.py
@@ -1,83 +1,12 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-"""This is the brute force solution for the client"""
+"""Actions"""
 
 import csv
-import itertools
-
-# Change this value to match the client's maximum amount to invest
-AMOUNT_MAX = 500
 
 
-class Action:
-    """This class represents an action"""
-    count = 0
-
-    def __init__(self, name, price, renta):
-        Action.count += 1
-        self.name = name
-        self.price = price
-        self.renta = renta
-        self.id = Action.count
-
-    def __str__(self):
-        return (
-            f"|{self.name:20} | prix: {self.price:20} | "
-            f"rentabilité: {self.renta_num:20.2f}|")
-
-    def __repr__(self):
-        return f"<Action {self.name}>"
-
-    @property
-    def renta_num(self):
-        return self.price*self.renta/100
-
-
-class Lot:
-    """This class represents a lot of actions"""
-    count = 0
-
-    def __init__(self, *args):
-        Lot.count += 1
-        self.name = f"lot n°{Lot.count:10} "
-        self.actions = []
-        for arg in args:
-            self.actions.append(arg)
-
-    def __str__(self):
-        return (
-            f"{self.name:20} | prix: {self.total_price:10} | "
-            f"rentabilité numéraire: {self.total_renta:10.2f}"
-        )
-
-    def __repr__(self):
-        return f"<Lot {self.name}>"
-
-    def show_actions(self):
-        for action in self.actions:
-            print(action)
-
-    @property
-    def total_price(self):
-        total = 0
-        for action in self.actions:
-            total += action.price
-        return total
-
-    @property
-    def total_renta(self):
-        rentability = 0
-        for action in self.actions:
-            rentability += action.price*action.renta/100
-        return rentability
-
-
-titles = []
-traceback = []
-
-
-# Get the actions from the datas.csv file
+# verify the actions from the datas.csv file
 def valid_data(data):
     if len(data) != 3:
         return False
@@ -86,54 +15,61 @@ def valid_data(data):
     return True
 
 
-with open("datas.csv", "r") as f:
-    datas = csv.reader(f)
-    next(datas)  # skip the header
-    for data in datas:
-        if valid_data(data):
-            titles.append(Action(data[0], float(data[1]), float(data[2])))
+def get_best(reste, n):
+    counter[0] += 1
+    if n == 0 or reste <= 0:
+        return 0, []
+    if prices[n-1] > reste:
+        # the actions price is too high, let's go to the next one
+        return get_best(reste, n-1)
+
+    # 2 cases:
+    # take
+    renta_take, path_take = get_best(reste-prices[n-1], n-1)
+    renta_take += renta[n-1]
+    path_take = path_take + [actions[n-1]]
+
+    # skip
+    renta_skip, path_skip = get_best(reste, n-1)
+
+    if max(renta_take, renta_skip) == renta_take:
+        return renta_take, path_take
+    else:
+        return renta_skip, path_skip
 
 
-def get_possibilities(titles):
-    """Returns a list of all possible combinations"""
-    number_of_actions = len(titles)
-    all_possibilities = []
-    for i in range(1, (number_of_actions+1)):
-        possibilities = list(itertools.combinations(titles, i))
-        all_possibilities.extend(possibilities)
-    return all_possibilities
+if __name__ == "__main__":
 
+    actions = []
+    prices = []
+    renta = []
 
-def get_best(all_results):
-    """Returns the best lot possible"""
-    best_renta = 0
-    best_lot = None
-    for line in all_results:
-        line = list(line)
-        lot = Lot(*line)
-        if lot.total_price <= AMOUNT_MAX:
-            if lot.total_renta > best_renta:
-                best_renta = lot.total_renta
-                best_lot = lot
-    return best_lot
+    # load datas from the file
+    with open("datas.csv", "r") as f:
+        datas = csv.reader(f)
+        next(datas)  # skip the header
+        for data in datas:
+            if valid_data(data):
+                actions.append(data[0])
+                price = int(float(data[1])*100)
+                prices.append(price)
+                rent_absolute = int(price*float(data[2]))
+                renta.append(rent_absolute)
 
+    counter = [0]
 
-def print_best_lot(lot):
-    """Displays the best lot"""
-    result = (
-        f"La meilleure rentabilité possible pour un "
-        f"investissement maximum de {AMOUNT_MAX} Euros s'obtient avec "
-        f"le lot suivant :\n"
-        f"prix : {lot.total_price}, rentabilité numéraire "
-        f"de {lot.total_renta:.2f}\n"
-        )
-    for action in lot.actions:
-        result += f"\n{action}"
-    with open("best_lot.txt", "w") as f:
-        f.write(result)
-    print(result)
+    MAX = 50000  # Max amount in cents
 
-
-all_results = get_possibilities(titles)
-best_lot = get_best(all_results)
-print_best_lot(best_lot)
+    renta, lot = get_best(MAX, len(actions))
+    renta_result = renta/10000
+    print(f"Rentabilité numeraire du lot: {renta_result}")
+    print("Composition du lot:")
+    for i in lot:
+        print(f"{i}")
+    price = 0
+    for action in lot:
+        index = actions.index(action)
+        price += prices[index]
+    price_result = price/100
+    print("cout total du lot: ", price_result)
+    print("iterations effectuées: ", counter[0])
--- optimized.py
+++ optimized.py
@@ -1,3 +1,84 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+"""Actions with memoization"""
+
+import csv
+import sys
+
+
+# verify the actions from the datas.csv file
+def valid_data(data):
+    if len(data) != 3:
+        return False
+    if float(data[1]) <= 0 or float(data[2]) <= 0:
+        return False
+    return True
+
+
+def get_best(reste, n, memo):
+    counter[0] += 1
+    if (reste, n) in memo:
+        return memo[(reste, n)]
+    if n == 0 or reste <= 0:
+        return 0, []
+    if prices[n-1] > reste:
+        # the actions price is too high, let's go to the next one
+        return get_best(reste, n-1, memo)
+
+    # 2 cases:
+    # take
+    renta_take, path_take = get_best(reste-prices[n-1], n-1, memo)
+    renta_take += renta[n-1]
+    path_take = path_take + [actions[n-1]]
+
+    # skip
+    renta_skip, path_skip = get_best(reste, n-1, memo)
+
+    # best branch selection, memo and return
+    if max(renta_take, renta_skip) == renta_take:
+        memo[(reste, n)] = (renta_take, path_take)
+        return renta_take, path_take
+    else:
+        memo[(reste, n)] = (renta_skip, path_skip)
+        return renta_skip, path_skip
+
+
+if __name__ == "__main__":
+
+    actions = []
+    prices = []
+    renta = []
+
+    # load datas from the file
+    with open("datas.csv", "r") as f:
+        datas = csv.reader(f)
+        next(datas)  # skip the header
+        for data in datas:
+            if valid_data(data):
+                actions.append(data[0])
+                price = int(float(data[1])*100)
+                prices.append(price)
+                rent_absolute = int(price*float(data[2]))
+                renta.append(rent_absolute)
+
+    counter = [0]
+
+    MAX = 50000  # Max amount in cents
+    memo = {}
+
+    renta, lot = get_best(MAX, len(actions), memo)
+    renta_result = renta/10000
+    print("Composition du lot sélectionné:")
+    for i in lot:
+        print(f"{i}")
+    price = 0
+    for action in lot:
+        index = actions.index(action)
+        price += prices[index]
+    price_result = price/100
+    print(f"cout total du lot: {price_result:_.2f}")
+    print(f"Rentabilité numeraire du lot: {renta_result:_.2f}")
+    print("\nInfos systeme:")
+    print(f"solicitation CPU: {counter[0]:_}")
+    print(f"taille du cache utilisé: {sys.getsizeof(memo)/1_000_000:_.2f} Mo")
